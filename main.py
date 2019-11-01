8c3fc5774952380cea457119068afcf079063d2b
jackleonard <jck.leonard@gmail.com>
2019-11-01 22:36:05 +0000

    -> Main.py
    Initialised Class Objects

--- main.py
+++ main.py
@@ -8,3 +8,26 @@
 
 ###################################################
 '''
+
+# ↓ Document Imports
+from participant import Participant, trialParticipant
+from filemanager import FileManager, DeckFileManager
+
+# ↓  Psychopy/External Dependency Imports
+from psychopy.visual import Window, TextStim
+import psychopy.visual
+from psychopy import event
+from psychopy import core
+
+# -> Instantiating Classes
+
+participantA = trialParticipant()
+deckA = DeckFileManager()
+deckB = DeckFileManager()
+deckC = DeckFileManager()
+deckD = DeckFileManager()
+
+
+
+
+
