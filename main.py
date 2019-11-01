e67bf44845f0818b55e2053e2159fd7f8efbf6e6
jackleonard <jck.leonard@gmail.com>
2019-11-01 22:28:08 +0000

    Initialized Main.py
    Rooibos

---
+++ main.py
@@ -0,0 +1,10 @@
+'''
+
+################## MAIN.PY ########################
+      © Jack Leonard 2019
+      117372043 - Iowa Gambling Task Project
+
+            Main Code & Psychopy based GUI
+
+###################################################
+'''
