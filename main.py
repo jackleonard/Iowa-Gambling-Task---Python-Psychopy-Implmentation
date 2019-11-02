ed88ea410947608fc21d5ad96e16a750dfe737d2
jackleonard <jck.leonard@gmail.com>
2019-11-02 22:53:56 +0000

    -> Main.py Created Static GUI with PsychoPy
    Added Title Text elements for winnings, losses etc
    Created titles for ABCD Decks
    
    Signed-off-by: jackleonard <jck.leonard@gmail.com>

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from participant import Participant, trialParticipant
 from filemanager import FileManager, DeckFileManager
 
 #       ↓  Psychopy/External Dependency Imports
-from psychopy.visual import Window, TextStim
+from psychopy.visual import Window, TextStim, ShapeStim
 import psychopy.visual
 from psychopy import event
 from psychopy import core
@@ -28,30 +28,124 @@ deckC = DeckFileManager()
 deckD = DeckFileManager()
 
 #       ✎ Psychopy GUI
-'''
+
 #todo: figure out why text isn't rendering and or centred
 
 display = Window(
-    size=(600,400),
+    size=(1080,810),
     units="pix",
-    color="#06070a"
+    color="#06070a",
 )
 
-#create a text object
-t1 = TextStim(
+#create "Winnings" Title Text object
+winningsTitleText = TextStim(
     win=display,
-    pos=[0,0], #note this value can be a list or a tuple
-    text="Hello world!!",
-    color="#27e4c9",
+    pos=(0,380), #note this value can be a list or a tuple
+    text="Winnings:",
+    font='Futura',
+    bold=True,
+    alignHoriz='center',
+    color="#4A5EB9",
+    height=25,
+)
+
+#create "Amount Won" subtitle text
+amountWonSubtitleText = TextStim(
+    win=display,
+    pos=(-400,290), #note this value can be a list or a tuple
+    text="Amount Won:",
+    font='Futura',
+    bold=True,
+    alignHoriz='center',
+    color="#4A5EB9",
+    height=15,
+)
+
+#create "Amount Lost" subtitle text
+amountLostSubtitleText = TextStim(
+    win=display,
+    pos=(400,290), #note this value can be a list or a tuple
+    text="Amount Lost:",
+    font='Futura',
+    bold=True,
+    alignHoriz='center',
+    color="#4A5EB9",
+    height=15,
+)
+
+#create Deck A Title Text
+deckATitleText = TextStim(
+    win=display,
+    pos=(-300,-20), #note this value can be a list or a tuple
+    text="A",
+    font='Futura',
+    alignHoriz='center',
+    color="#FFFFFF",
+    height=45,
+)
+
+#create Deck B Title Text
+deckBTitleText = TextStim(
+    win=display,
+    pos=(-100,-20), #note this value can be a list or a tuple
+    text="B",
+    font='Futura',
+    alignHoriz='center',
+    color="#FFFFFF",
+    height=45,
+)
+
+#create Deck C Title Text
+deckCTitleText = TextStim(
+    win=display,
+    pos=(100,-20), #note this value can be a list or a tuple
+    text="C",
+    font='Futura',
+    alignHoriz='center',
+    color="#FFFFFF",
+    height=45,
+)
+
+#create Deck D Title Text
+deckDTitleText = TextStim(
+    win=display,
+    pos=(300,-20), #note this value can be a list or a tuple
+    text="D",
+    font='Futura',
+    alignHoriz='center',
+    color="#FFFFFF",
+    height=45,
 )
 
 
-t1.draw() #draw the text
+
+#Creating the Main Header  Section
+div = psychopy.visual.Rect(
+    size=(2160, 540), #todo: for some reason resolution is being rendered @2x - clarify this with Laura
+    units="pix",
+    fillColor="#0a0d1c",
+    lineColor='#0a0d1c',
+    win=display,
+    pos=(0, 350),
+
+)
+
+
+
+#Draw Elements to the Canvas
+div.draw()
+winningsTitleText.draw() #draw the text
+amountWonSubtitleText.draw()
+amountLostSubtitleText.draw()
+deckATitleText.draw()
+deckBTitleText.draw()
+deckCTitleText.draw()
+deckDTitleText.draw()
 display.flip() #clear the canvas and place new 'drawable' elements on the canvas
 core.wait(25) #wait 7 seconds
 display.close() #close the display
 
-'''
+
 
 #       ♔ Main Code
 
